d096d9ff3ef9609e61a16648d718690c97115345
Zerberuss <an-schneider@hotmail.com>
2015-11-26 22:12:55 +0100

    PreFinal Commit -> tests deletet -.-

--- src/main/scala/fhj/swengb/assignments/tree/aschneider/TreeAssignment.scala
+++ src/main/scala/fhj/swengb/assignments/tree/aschneider/TreeAssignment.scala
@@ -43,8 +43,7 @@ object Graph {
     */
   def traverse[A, B](tree: Tree[A])(convert: A => B): Seq[B] = tree match{
     case Node(values) => Seq(convert(values))     //wenn node convertieren und zu seq machen
-    case Branch(left, right) => traverse(left)(convert)
-                                traverse(right)(convert)
+    case Branch(left, right) => traverse(left)(convert) ++ traverse(right)(convert)
 
   }
 
